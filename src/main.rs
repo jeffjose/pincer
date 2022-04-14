246e373b30d53a0b2a64f2213087072d30b7590c
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-04-13 21:54:15 -0700

    pass url with query params properly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use poem::{get, handler, listener::TcpListener, web::Html, web::Json, web::Path, Route, Server};
-use reqwest;
+use poem::{
+    error::BadRequest, get, handler, listener::TcpListener, web::Json, web::Path, Result, Route,
+    Server,
+};
+use reqwest::{self, Method};
 
 use serde::Serialize;
 
@@ -9,13 +12,40 @@ struct Response {
 }
 
 #[handler]
-async fn proxy(Path(url): Path<String>) -> Html<String> {
-    //format!("{}", name)
+async fn proxy(req: &poem::Request, _body: poem::Body) -> Result<poem::Response> {
+    //let fullpath = format!("https://{url}");
+    //let result = reqwest::get(fullpath).await.unwrap().text().await.unwrap();
 
-    let fullpath = format!("https://{url}");
-    let result = reqwest::get(fullpath).await.unwrap().text().await.unwrap();
+    //let path = req.path_params::<String>()?;
 
-    Html(result)
+    let path = req
+        .uri()
+        .path()
+        .replace("/proxy", "")
+        .replace("https://", "")
+        .replace("http://", "");
+    let query = match req.uri().query() {
+        Some(q) => format!("?{}", q),
+        None => String::from(""),
+    };
+
+    let url = format!("https://{path}{query}",);
+
+    println!("[REQ]: {url}");
+
+    let cli = reqwest::Client::new();
+    let resp = cli
+        .request(Method::GET, url)
+        .send()
+        .await
+        .map_err(BadRequest)?;
+
+    let mut r = poem::Response::default();
+    r.set_status(resp.status());
+    *r.headers_mut() = resp.headers().clone();
+    r.set_body(resp.bytes().await.map_err(BadRequest)?);
+
+    Ok(r)
 }
 
 #[handler]
@@ -26,7 +56,7 @@ async fn json(Path(name): Path<String>) -> Json<Response> {
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let app = Route::new()
-        .at("/proxy/*url", get(proxy))
+        .at("/proxy/*url", proxy)
         .at("/json/:name", get(json));
     Server::new(TcpListener::bind("127.0.0.1:3000"))
         .name("pincer")
