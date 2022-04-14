3c8b0369794978002f3d9f94f2fdba123bccca1e
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-04-13 17:14:06 -0700

    return json

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+use poem::{get, handler, listener::TcpListener, web::Json, web::Path, Route, Server};
+
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Response {
+    name: String,
+}
 
 #[handler]
-fn hello(Path(name): Path<String>) -> String {
+async fn hello(Path(name): Path<String>) -> String {
     format!("hello: {}", name)
+}
+
+#[handler]
+async fn json(Path(name): Path<String>) -> Json<Response> {
+    Json(Response { name })
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app = Route::new().at("/reddit/:name", get(hello));
+    let app = Route::new()
+        .at("/reddit/:name", get(hello))
+        .at("/json/:name", get(json));
     Server::new(TcpListener::bind("127.0.0.1:3000"))
-        .name("hello-world")
+        .name("pincer")
         .run(app)
         .await
 }
