2f53e1e22cddabf0b439ec63326312aa3c8e6f31
Jeffrey Jose <jeffjosejeff@gmail.com>
2022-04-13 17:02:23 -0700

    initial add

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/reddit/:name", get(hello));
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .name("hello-world")
+        .run(app)
+        .await
+}
